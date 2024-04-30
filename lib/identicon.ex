c67ff79b2d97ba7b8660aedd5ecb3aac3a6cae01
brad preston <bap5393@gmail.com>
2024-04-30 18:17:48 -0400

    Add main and hash_input methods to Identicon module

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,2 +1,12 @@
 defmodule Identicon do
+  def main(input) do
+    input
+    |> hash_input()
+  end
+
+  def hash_input(input) do
+    # :crypto.hash returns an md5 hash, which :binary.bin_to_list takes as an argument
+    :crypto.hash(:md5, input)
+    |> :binary.bin_to_list()
+  end
 end
