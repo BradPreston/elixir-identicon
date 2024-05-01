e5b84a1d030f0acbe89b475bb5c671f9f0625d32
brad preston <bap5393@gmail.com>
2024-05-01 18:19:55 -0400

    Pattern match the image as an argument and update the struct with the rgb

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -5,15 +5,18 @@ defmodule Identicon do
     |> pick_color()
   end
 
-  def pick_color(image) do
+  ## Pattern match the image as an argument
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
     # image is referencing the %Identicon.Image{} struct
 
     # Using pattern matching, we are checking that
     # 1. image is an %Identicon.Image{},
     # 2. the Image struct has a hex property, and
     # 3. grabs the first 3 variables in the image (hex list) and ignores the rest
-    %Identicon.Image{hex: [r, g, b | _tail]} = image
-    [r, g, b]
+
+    # Create a new Image (to prevent modifying existing data) and update the color value
+    # Color is set with a tuple instead of a list so that each index has meaning
+    %Identicon.Image{image | color: {r, g, b}}
   end
 
   def hash_input(input) do
