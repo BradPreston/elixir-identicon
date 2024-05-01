875d6d1cacd02dfd733fc90454204a6b51aa2084
brad preston <bap5393@gmail.com>
2024-04-30 20:42:36 -0400

    Add pick_color method to Identicon module

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -2,6 +2,18 @@ defmodule Identicon do
   def main(input) do
     input
     |> hash_input()
+    |> pick_color()
+  end
+
+  def pick_color(image) do
+    # image is referencing the %Identicon.Image{} struct
+
+    # Using pattern matching, we are checking that
+    # 1. image is an %Identicon.Image{},
+    # 2. the Image struct has a hex property, and
+    # 3. grabs the first 3 variables in the image (hex list) and ignores the rest
+    %Identicon.Image{hex: [r, g, b | _tail]} = image
+    [r, g, b]
   end
 
   def hash_input(input) do
