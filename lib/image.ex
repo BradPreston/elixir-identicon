0e8967a1ac5ba7f9e8e6dfc9845bd9c6109f6547
brad preston <bap5393@gmail.com>
2024-05-01 21:32:45 -0400

    Add grid to the Image struct

--- lib/image.ex
+++ lib/image.ex
@@ -1,5 +1,5 @@
 defmodule Identicon.Image do
   # This struct is used to store all of the image information in our application
   # hex is used to store a list of integers that represent hexidecimal values
-  defstruct hex: nil, color: nil
+  defstruct hex: nil, color: nil, grid: nil
 end
